024e50538e1ea20ce3df6ad74508daed048b680b
Mustafa Sarıtaş <mstfsrts@gmail.com>
2024-10-05 20:16:18 +0200

    NodeJs Week3 PrepExercise Completed

--- week3/prep-exercise/server/app.js
+++ week3/prep-exercise/server/app.js
@@ -1,13 +1,14 @@
 import express from 'express';
-// TODO Use below import statement for importing middlewares from users.js for your routes
-// TODO import { ....... } from "./users.js";
+import { register, login, getProfile, logout } from './users.js';
 
-let app = express();
-
+const app = express();
 app.use(express.json());
-// TODO: Create routes here, e.g. app.post("/register", .......)
 
-// Serve the front-end application from the `client` folder
+app.post('/auth/register', register);
+app.post('/auth/login', login);
+app.get('/auth/profile', getProfile);
+app.post('/auth/logout', logout);
+
 app.use(express.static('client'));
 
 app.listen(3000, () => {
--- week3/prep-exercise/server/users.js
+++ week3/prep-exercise/server/users.js
@@ -1,12 +1,58 @@
-import newDatabase from './database.js'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import newDatabase from './database.js';
 
-// Change this boolean to true if you wish to keep your
-// users between restart of your application
-const isPersistent = false
-const database = newDatabase({isPersistent})
+const SECRET_KEY = 'your_jwt_secret_key';
+const database = newDatabase({ isPersistent: false });
 
-// Create middlewares required for routes defined in app.js
-// export const register = async (req, res) => {};
+export const register = async (req, res) => {
+  const { username, password } = req.body;
 
-// You can also create helper functions in this file to help you implement logic
-// inside middlewares
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Username and password are required' });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = database.create({ username, password: hashedPassword });
+
+  res.status(201).json({ id: user.id, username: user.username });
+};
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = database.getById(username);
+
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ message: 'Invalid credentials' });
+  }
+
+  const token = jwt.sign({ id: user.id }, SECRET_KEY, { expiresIn: '1h' });
+  res.status(201).json({ token });
+};
+
+export const getProfile = (req, res) => {
+  const token = req.headers.authorization?.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    const user = database.getById(decoded.id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json({ username: user.username });
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+export const logout = (req, res) => {
+  res.status(204).send();
+};
